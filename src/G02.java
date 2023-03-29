fe3fd1b553e6831aae3494d45203fc55092c73a3
HakkiKadir <hakkikadirozbek@gmail.com>
2023-03-29 21:55:47 +0300

    4.Gun

---
+++ src/G02.java
@@ -0,0 +1,20 @@
+public class G02 {
+    /*
+    GitHub'ta projelerimizin üzerinde çalışırken temelde 3 adımda işlem yapıyoruz
+
+    1- Projelerimizi/Dosyalarımızı git ile ilişkilendiririz
+        -> git init
+    2-Dosyamızda/Projemizde bulunanları versiyon oluşturuyorduk
+        -> git add .
+        -> git commit -m "versiyonAdi"
+
+    3-GitHub'a yollama/entegrasyonu
+        -> git remote add origin repoUrlAdresi
+        -> already origin
+        -> git remote set-url repoUrlAdresi
+
+        -> git push
+
+
+     */
+}
